03f2471a887f96a690de813d231a43e337fd4591
fenec_fox <atimevil@gmail.com>
2024-04-05 04:30:29 +0900

    3차원 배열 테스트_2

--- popup.js
+++ popup.js
@@ -1,45 +1,85 @@
 // 레이어 팝업 열기
-document.getElementById('showPopup').addEventListener('click', function() {
+document.getElementById('showPopup').addEventListener('click', function () {
     document.getElementById('popupContainer').style.display = 'block';
 });
 
 // 레이어 팝업 닫기
-document.getElementById('closePopup').addEventListener('click', function() {
+document.getElementById('closePopup').addEventListener('click', function () {
     document.getElementById('popupContainer').style.display = 'none';
 });
 
 function getBaseType() {
     var obj = {
-        "위스키" : {
-            'a' : {
-                '10도 이하' ,
-                '10 ~ 20도' ,
-                '20 ~ 30도' ,
-                '30 ~ 40도' ,
-                '40도 이상' ,
-                }, 
-            'b' : '올드 패션드',
-            'c' : '뉴욕 사워'
+        "위스키": {
+            '10도 이하':
+                [
+                    '칵테일 이름1',
+                    '칵테일 이름2',
+                    '칵테일 이름3',
+                    '칵테일 이름4',
+                    '칵테일 이름5'
+                ]
+            ,
+            '10도 ~ 20도':
+                [
+                    '칵테일 이름1',
+                    '칵테일 이름2',
+                    '칵테일 이름3',
+                    '칵테일 이름4',
+                    '칵테일 이름5'
+                ]
+            ,
+            '20도 ~ 30도':
+                [
+                    '칵테일 이름1',
+                    '칵테일 이름2',
+                    '칵테일 이름3',
+                    '칵테일 이름4',
+                    '칵테일 이름5'
+                ]
         },
-        "진" : {
-            'a' : '실버 불렛',
-            'b' : '예시 1',
-            'c' : '예시 2'
+        "진": {
+            '10도 이하':
+                [
+                    '칵테일 이름1',
+                    '칵테일 이름2',
+                    '칵테일 이름3',
+                    '칵테일 이름4',
+                    '칵테일 이름5'
+                ]
+            ,
+            '10도 ~ 20도':
+                [
+                    '칵테일 이름1',
+                    '칵테일 이름2',
+                    '칵테일 이름3',
+                    '칵테일 이름4',
+                    '칵테일 이름5'
+                ]
+            ,
+            '20도 ~ 30도':
+                [
+                    '칵테일 이름1',
+                    '칵테일 이름2',
+                    '칵테일 이름3',
+                    '칵테일 이름4',
+                    '칵테일 이름5'
+                ]
         }
     }
     return obj;
 }
 
-function Base_value(val){
-    if(val == "Whiskey"){
+function Base_value(val) {
+    if (val == "Whiskey") {
         //Whiskey.style.display = 'inline';
         //Jin.style.display = 'none';
-    } else if(val == "Jin"){
+    } else if (val == "Jin") {
         //Whiskey.style.display = 'none';
         //Jin.style.display = 'none';
     }
 }
 
-function alcohol_value(val){
+function alcohol_value(val) {
 
 }
