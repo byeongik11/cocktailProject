27656500a3905794d6462a00145df8884bbb65f6
fenec_fox <atimevil@gmail.com>
2024-04-04 17:37:55 +0900

    데이터 추가

--- popup.js
+++ popup.js
@@ -8,6 +8,22 @@ document.getElementById('closePopup').addEventListener('click', function() {
     document.getElementById('popupContainer').style.display = 'none';
 });
 
+function getBaseType() {
+    var obj = {
+        "위스키" : {
+            'a' : '러스티 네일',
+            'b' : '올드 패션드',
+            'c' : '뉴욕 사워'
+        },
+        "진" : {
+            'a' : '실버 불렛',
+            'b' : '예시 1',
+            'c' : '예시 2'
+        }
+    }
+    return obj;
+}
+
 function Base_value(val){
     if(val == "Whiskey"){
         //Whiskey.style.display = 'inline';
